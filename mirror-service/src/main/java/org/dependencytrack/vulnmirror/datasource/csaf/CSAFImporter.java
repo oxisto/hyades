f6f9c2e42260e8c14402e8aed7dd2e157f5b034c
Christian Banse <oxisto@clouditor.io>
2024-03-22 20:48:54 +0100

    importer and config

---
+++ mirror-service/src/main/java/org/dependencytrack/vulnmirror/datasource/csaf/CSAFImporter.java
@@ -0,0 +1,9 @@
+package org.dependencytrack.vulnmirror.datasource.csaf;
+
+import org.cyclonedx.proto.v1_4.Bom;
+
+public class CSAFImporter {
+    public static Bom parse(CSAFConfig config) {
+        return null;
+    }
+}
--- mirror-service/src/main/java/org/dependencytrack/vulnmirror/datasource/csaf/CSAFMirror.java
+++ mirror-service/src/main/java/org/dependencytrack/vulnmirror/datasource/csaf/CSAFMirror.java
@@ -1,12 +1,40 @@
 package org.dependencytrack.vulnmirror.datasource.csaf;
 
 import org.dependencytrack.vulnmirror.datasource.AbstractDatasourceMirror;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.ExecutorService;
 
 public class CSAFMirror extends AbstractDatasourceMirror<CSAFMirrorState> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CSAFMirror.class);
+
+    private final ExecutorService executorService;
+    private final CSAFConfig config;
+
+    public CSAFMirror(ExecutorService executorService, CSAFConfig config) {
+        this.executorService = executorService;
+        this.config = config;
+    }
+
     @Override
     public Future<?> doMirror(String ecosystem) {
-        return null;
+        return executorService.submit(() -> {
+        // do the actual import based on the config
+            try {
+                doImport(config);
+            } catch (Exception e) {
+                LOGGER.error("Error occured while importing CSAF", e);
+            }
+        });
+    }
+
+    private void doImport(CSAFConfig config) throws ExecutionException, InterruptedException {
+        // fetch CSAF form URL, parse it to a Bom
+        Bom bom = CSAFImporter.parse(config.url);
+        publishIfChanged(bom);
     }
 }
